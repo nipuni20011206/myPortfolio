b80f8c03fa489d73b17c7b7ef8ebef8b6eebd280
nipuni20011206 <nipunibandara73@gmail.com>
2025-01-21 15:04:12 +0530

    Initial commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Qjc0fHNUJggAlhblc4JAPKwLc3RcqoUAc+uy81yXnig=\"\n}"
---
+++ .next/static/chunks/app_page_0a2b19.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_0a2b19.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_0a2b19.js",
+  "chunks": [
+    "static/chunks/_b323e3._.js",
+    "static/chunks/node_modules_8ed55d._.js"
+  ],
+  "source": "dynamic"
+});
