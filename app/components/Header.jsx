05fb26b4b90bbf012c44b48f21af1a41ce2ba8a2
nipuni20011206 <nipunibandara73@gmail.com>
2025-05-20 12:11:05 +0530

    designed

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -26,7 +26,9 @@ const Header = () => {
         whileInView={{y: 0, opacity: 1}}
         transition={{duration: 0.8, delay: 0.5}}
         className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
-            Web developer (FulStack - Java) | Undergraduate at SLIIT
+            Web Developer | <br>
+            Undergraduate at SLIIT
+            </br>
         </motion.h1>
         <motion.p 
         initial={{opacity: 0}}
